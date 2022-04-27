6300e113471fee6a473e9301d87eaa43179bd8a4
lan <bryanmasca3@gmail.com>
2022-04-27 15:37:07 -0500

    config env and heroku enviroment

--- src/database.js
+++ src/database.js
@@ -1,4 +1,10 @@
 const {Pool} = require('pg')
+require("dotenv").config();
+
+
+const isProduction = process.env.NODE_ENV === "production";
+/*
+
 const pool = new Pool({
   user: 'admindbsm',
   host: 'localhost',
@@ -6,5 +12,13 @@ const pool = new Pool({
   database: 'dbsanmartin1',
   port:5432
 })
+*/
 
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 module.exports = {pool}
